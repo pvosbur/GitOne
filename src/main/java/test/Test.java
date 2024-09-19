67be400f4cc6f6f7b1d1a9accafe8ab0917d713c
Peter VosBurgh <pvosbur@protonmail.com>
2024-09-19 10:47:39 -0400

    initial load

---
+++ src/main/java/test/Test.java
@@ -0,0 +1,23 @@
+package test;
+
+/*
+============================================================================================
+
+    Source File Name: Transaction.java
+
+    Author:           petervosburgh
+    
+    Date Generated:   9/14/24
+
+    Time Generated:   9:08 AM
+
+============================================================================================
+*/
+public class Test
+{
+  public static void main(String[] args)
+  {
+    //System.out.println("In Test");
+    
+  }
+}
---
+++ web/demo/Demo.js
@@ -0,0 +1,18 @@
+/*
+ * Created by User: petervosburgh
+ * Date: 9/14/24
+ * Time: 8:59 AM
+ * 
+ */
+
+function Demo()
+{
+  configObject();
+
+  function configObject()
+  {
+    $("#h1").text( "Demo One");
+  }
+}
+
+export default Demo;
